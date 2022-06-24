ed58c755213be51d6e608f8dbe49dbd5982cdf9b
hameedah2020 <67450419+hameedah2020@users.noreply.github.com>
2022-06-24 20:49:53 +0100

    final

--- I4G007026ZZE/blog/views.py
+++ I4G007026ZZE/blog/views.py
@@ -1,32 +1,35 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views import generic
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 from .models import Post
-
-class PostListView(generic.ListView):
+from django.urls import reverse_lazy
+class PostListView(ListView):
     model = Post
     template_name= "blog/post_list.html"
 
-class PostCreateView(generic.CreateView): 
+class PostCreateView(CreateView): 
       model = Post
-      fields = “__all__”
-      success_url  = reverse_lazy (“blog:all”)
+      fields = "__all__"
+      success_url  = reverse_lazy ("blog:all")
       template_name = "blog/post_form.html"
 
-class PostDetailView(generic.DetailView):
+class PostDetailView(DetailView):
         model = Post
         template_name="blog/post_detail.html"
     
-class PostUpdateView(generic.UpdateView):
+class PostUpdateView(UpdateView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
     template_name = "blog/post.form.html"
 
-class PostDeleteView(generic.DeleteView):
+class PostDeleteView(DeleteView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
     template_nmae = "blog/post_confirm_delete.html"
 
  
